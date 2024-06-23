ad60e3e03358f35a6ceb086abd36a2f9c64e3357
Eugeny Dementyev <eugeny.dementev@ya.ru>
2024-06-23 15:33:19 +0800

    feat: export locking actino factory

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 export { IAction, ActionClass, QueueAction, Branches, QueueContext } from './types.js';
 export { QueueRunner, EndListener } from './runner.js'
 export { AsyncQueue, QueueOpts } from './queue.js';
-export { Action, Options } from './action.js';
+export { Action, Options, lockingClassFactory } from './action.js';
 export { util } from './utils.js';
